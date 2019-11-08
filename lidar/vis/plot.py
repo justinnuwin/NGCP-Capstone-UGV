733a8386bba1cfaf3bfb5ec7fc6547098c256807
Alexander Garcia <haloalex68@gmail.com>
2019-11-08 11:43:22 -0800

    modified visualizer to show 10 revs of data

--- lidar/vis/plot.py
+++ lidar/vis/plot.py
@@ -2,10 +2,22 @@ import matplotlib.pyplot as plt
 import numpy
 import pandas as pd
 
-data = pd.read_csv("data.csv")
-data['rad']  = data['theta'] * 3.14 / 180
-#plt.polar(data['rad'], data['dist'], linewidth=1)
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='polar')
-c = ax.scatter(data['rad'], data['dist'], s = 1)
+data = pd.read_csv("data1.csv")
+print(data)
+indices = [0]
+oldTheta = -1
+for i, j in data.iterrows():
+    if(j["theta"] < oldTheta):
+        indices.append(i)
+    oldTheta = j["theta"]
+indices.append(i+1)
+
+a = 0
+for i in range(0, len(indices) - 1):
+    tempData = data.loc[indices[a] : indices[a + 1] - 1]
+    tempData['rad']  = tempData['theta'] * 3.14 / 180
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='polar')
+    c = ax.scatter(tempData['rad'], tempData['dist'], s = 1)
+    a += 1
 plt.show()
