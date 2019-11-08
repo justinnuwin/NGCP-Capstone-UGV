d0e2c8e714d45f0959ae478b2661150e11459c24
Justin Nguyen <justinnuwin@gmail.com>
2019-11-08 10:28:02 -0800

    Update plot to take in a file from argv and plot in the correct direction

--- lidar/vis/plot.py
+++ lidar/vis/plot.py
@@ -1,11 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy
 import pandas as pd
+import sys
 
-data = pd.read_csv("data.csv")
+data = pd.read_csv(sys.argv[1])
 data['rad']  = data['theta'] * 3.14 / 180
 #plt.polar(data['rad'], data['dist'], linewidth=1)
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='polar')
+ax.set_theta_zero_location("N")
+ax.set_theta_direction(-1)
 c = ax.scatter(data['rad'], data['dist'], s = 1)
 plt.show()
