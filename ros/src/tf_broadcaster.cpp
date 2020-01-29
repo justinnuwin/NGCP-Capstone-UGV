0f1f55768df601ce7766e10877c798e826561ea9
Gonzalo <garana@Gonzalos-MacBook-Pro.local>
2020-01-29 10:03:10 -0800

    converted vector values from cm to meters

--- ros/src/tf_broadcaster.cpp
+++ ros/src/tf_broadcaster.cpp
@@ -12,11 +12,11 @@ int main(int argc, char** argv){
   while(n.ok()){
     broadcaster.sendTransform(
       tf::StampedTransform(
-        tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(10.5, -10.5, 3.0)),   // cm
+        tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0.105, -0.105, 0.030)),   // m
         ros::Time::now(),"base", "lidar"));
     broadcaster.sendTransform(
       tf::StampedTransform(
-        tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(16, 8.5, 2.0)),   // cm
+        tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0.16, 0.085, 0.02)),   // m
         ros::Time::now(),"base", "camera"));
     r.sleep();
   }
