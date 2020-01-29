c9bac215d54ec179f1fd8f8847248665d2c19fdd
Gonzalo Arana <gonzalo.ar18@hotmail.com>
2020-01-29 14:47:51 -0800

    completed ros package with working tf_broadcaster and listeners for lidar and camera.

--- ros/src/bugv_tf/src/tf_listener.cpp
+++ ros/src/bugv_tf/src/tf_listener.cpp
@@ -2,6 +2,9 @@
 #include <geometry_msgs/PointStamped.h>
 #include <tf/transform_listener.h>
 
+void transformLidar(const tf::TransformListener& listener);
+void transformZed(const tf::TransformListener& listener);
+
 void transformPoint(const tf::TransformListener& listener){
   transformLidar(listener);
   transformZed(listener);
@@ -34,28 +37,28 @@ void transformLidar(const tf::TransformListener& listener){
 }
 
 void transformZed(const tf::TransformListener& listener){
-  //we'll create a point in the base_zed frame that we'd like to transform to the base_link frame
-  geometry_msgs::PointStamped zed_point;
-  zed_point.header.frame_id = "zed";
+  //we'll create a point in the base_camera frame that we'd like to transform to the base_link frame
+  geometry_msgs::PointStamped camera_point;
+  camera_point.header.frame_id = "camera";
 
   //we'll just use the most recent transform available for our simple example
-  zed_point.header.stamp = ros::Time();
+  camera_point.header.stamp = ros::Time();
 
   //just an arbitrary point in space
-  zed_point.point.x = 1.0;
-  zed_point.point.y = 0.2;
-  zed_point.point.z = 0.0;
+  camera_point.point.x = 1.0;
+  camera_point.point.y = 0.2;
+  camera_point.point.z = 0.0;
 
   try{
     geometry_msgs::PointStamped base_point;
-    listener.transformPoint("base", zed_point, base_point);
+    listener.transformPoint("base", camera_point, base_point);
 
-    ROS_INFO("base_zed: (%.2f, %.2f. %.2f) -----> base_link: (%.2f, %.2f, %.2f) at time %.2f",
-        zed_point.point.x, zed_point.point.y, zed_point.point.z,
+    ROS_INFO("base_camera: (%.2f, %.2f. %.2f) -----> base_link: (%.2f, %.2f, %.2f) at time %.2f",
+        camera_point.point.x, camera_point.point.y, camera_point.point.z,
         base_point.point.x, base_point.point.y, base_point.point.z, base_point.header.stamp.toSec());
   }
   catch(tf::TransformException& ex){
-    ROS_ERROR("Received an exception trying to transform a point from \"base_zed\" to \"base_link\": %s", ex.what());
+    ROS_ERROR("Received an exception trying to transform a point from \"base_camera\" to \"base_link\": %s", ex.what());
   }
 }
 
