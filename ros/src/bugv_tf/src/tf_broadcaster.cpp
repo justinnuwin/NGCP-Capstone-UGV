b6ae26c04d24cf93b2e45c9584f1e33eb122ec17
Justin Nguyen <justinnuwin@gmail.com>
2020-02-03 10:09:46 -0800

    set up more stuff for rtabmap

--- ros/src/bugv_tf/src/tf_broadcaster.cpp
+++ ros/src/bugv_tf/src/tf_broadcaster.cpp
@@ -13,11 +13,11 @@ int main(int argc, char** argv){
     broadcaster.sendTransform(
       tf::StampedTransform(
         tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0.105, -0.105, 0.030)),   // m
-        ros::Time::now(),"base", "lidar"));
+        ros::Time::now(),"base_link", "lidar"));
     broadcaster.sendTransform(
       tf::StampedTransform(
         tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0.16, 0.085, 0.02)),   // m
-        ros::Time::now(),"base", "camera"));
+        ros::Time::now(),"base_link", "zed_camera_center"));
     r.sleep();
   }
 }
