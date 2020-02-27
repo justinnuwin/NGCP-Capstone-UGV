40badda34f9fc1395e52578a18f15dc62c16769e
Justin Nguyen <justinnuwin@gmail.com>
2020-02-27 00:13:39 -0800

    Move example waypoint file. Update set waypoint command to be nicer

--- ros/scripts/set_wp.sh
+++ ros/scripts/set_wp.sh
@@ -1,4 +1,29 @@
-../src/mavros/mavros/scripts/mavwp load test_wp.qgc
+#!/bin/bash
 
-../src/mavros/mavros/scripts/mavwp show
-../src/mavros/mavros/scripts/mavwp setcur 0
+source ../devel/setup.bash
+
+function usage {
+    echo "usage: set_wp.sh [-h] filename"
+    echo "Options:"
+    echo "    --help, -h    Show this help text"
+    echo "    filename      QGC Waypoint file. See example_wp.qgc in project root"
+    exit
+}
+
+if [ $# = 0 ] || [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
+    usage
+fi
+
+if [ ! -f $1 ]; then
+    echo "$1: No such file"
+    exit -1
+else
+    filename=$(readlink -f $1)
+fi
+
+roscd mavros
+./scripts/mavwp clear
+./scripts/mavwp load $filename
+
+./scripts/mavwp show
+./scripts/mavwp setcur 0
